9da674434b5e74db51f182c9b9a1154ade76877a
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-08-24 19:26:53 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,85 +1,78 @@
-import express from "express";
-import http from "node:http";
-import createBareServer from "@tomphttp/bare-server-node";
-import path from "node:path";
-import * as dotenv from "dotenv";
-dotenv.config();
+"use strict";
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("uv-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("uv-address");
+/**
+ * @type {HTMLInputElement}
+ */
+const searchEngine = document.getElementById("uv-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
 
-const __dirname = process.cwd();
-const server = http.createServer();
-const app = express(server);
-const bareServer = createBareServer("/bare/");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const url = search(address.value, searchEngine.value);
+  if (localStorage.getItem('openiframe')==="false"){
+    if (localStorage.getItem('searchmode')==="proxy"){
+    
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
+      try {
+        await registerSW();
+      } catch (err) {
+        error.textContent = "Failed to register service worker.";
+        errorCode.textContent = err.toString();
+        throw err;
+      }
+        location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+      }else{
+        window.open(url)
+      }
+  }
+  if (localStorage.getItem('openiframe')==="true"){
+    if (localStorage.getItem('searchmode')==="proxy"){
+    
+
+      try {
+        await registerSW();
+      } catch (err) {
+        error.textContent = "Failed to register service worker.";
+        errorCode.textContent = err.toString();
+        throw err;
+      }
+        location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+      }else{
+        location.href = url
+      }
+  }
+ 
+  
+});
+
+function openURL(value) {
+  window.navigator.serviceWorker.register("/uv/sw.js", {
+    scope: __uv$config.prefix
   })
-);
+  .then(() => {
+    console.log("Service worker registration successful");
 
-app.use(express.static(path.join(__dirname, "routes")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "routes", "index.html"));
-});
-
-app.get("/photography", (req, res) => {
-  res.sendFile(path.join(__dirname, "routes", "search.html"));
-});
-
-app.get("/nature", (req, res) => {
-  res.sendFile(path.join(__dirname, "routes", "play.html"));
-});
-
-app.get("/forest", (req, res) => {
-  res.sendFile(path.join(__dirname, "routes", "widgetbot.html"));
-});
-
-app.get("/go", (req, res) => {
-  res.sendFile(path.join(__dirname, "routes", "go.html"));
-});
-
-app.get("/settings", (req, res) => {
-  res.sendFile(path.join(__dirname, "routes", "settings.html"));
-});
-
-app.get("/donate", (req, res) => {
-  res.sendFile(path.join(__dirname, "routes", "donate.html"));
-});
-
-app.get("/ocean", (req, res) => {
-  res.sendFile(path.join(__dirname, "routes", "apps.html"));
-});
-
-app.get("/404", (req, res) => {
-  res.sendFile(path.join(__dirname, "routes", "404.html"));
-});
-
-app.get("/*", (req, res) => {
-  res.redirect("/404");
-});
-
-// Bare Server
-server.on("request", (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
-});
-
-server.on("upgrade", (req, socket, head) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
-  }
-});
-
-server.on("listening", () => {
-  console.log(`Interstellar running at http://localhost:${process.env.PORT}`);
-});
-
-server.listen({
-  port: process.env.PORT,
-});
+    let url = value.trim();
+    if (!(url.startsWith("https://") || url.startsWith("http://"))) {
+      url = "https://" + url;
+    }
+    location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  })
+  .catch((error) => {
+    console.error("Failed to register service worker.", error);
+  });
+}
